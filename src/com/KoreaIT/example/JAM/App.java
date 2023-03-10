a589b41511520cb236dd4e22fbbd16b271181ac7
devCJH <http-@naver.com>
2023-03-10 15:52:22 +0900

    article modify 코드 정리

--- src/com/KoreaIT/example/JAM/App.java
+++ src/com/KoreaIT/example/JAM/App.java
@@ -77,26 +77,37 @@ public class App {
 						System.out.printf("%d	|	%s\n", article.id, article.title);
 					}
 				} else if (cmd.startsWith("article modify ")) {
-					System.out.println("== 게시물 수정 ==");
 					int id = Integer.parseInt(cmd.split(" ")[2]);
+					
+					SecSql sql = new SecSql();
+					
+					sql.append("SELECT COUNT(*)");
+					sql.append("FROM article");
+					sql.append("WHERE id = ?", id);
+					
+					int articlesCount = DBUtil.selectRowIntValue(conn, sql);
+					
+					if(articlesCount == 0) {
+						System.out.printf("%d번 글은 존재하지 않습니다\n", id);
+						continue;
+					}
+					
+					System.out.println("== 게시물 수정 ==");
 
 					System.out.printf("수정할 제목 : ");
 					String title = sc.nextLine();
 					System.out.printf("수정할 내용 : ");
 					String body = sc.nextLine();
-
-//					try {
-//						String sql = "UPDATE article";
-//						sql += " SET updateDate = NOW()";
-//						sql += ", title = '" + title + "'";
-//						sql += ", `body` = '" + body + "'";
-//						sql += " WHERE id = " + id + ";";
-//
-//						pstmt = conn.prepareStatement(sql);
-//						pstmt.executeUpdate();
-//					} catch (SQLException e) {
-//						System.out.println("에러: " + e);
-//					}
+					
+					sql = new SecSql();
+					
+					sql.append("UPDATE article");
+					sql.append("SET updateDate = NOW()");
+					sql.append(", title = ?", title);
+					sql.append(", `body` = ?", body);
+					sql.append("WHERE id = ?", id);
+					
+					DBUtil.update(conn, sql);
 
 					System.out.printf("%d번 글이 수정되었습니다\n", id);
 				}
--- src/com/KoreaIT/example/JAM/exception/SQLErrorException.java
+++ src/com/KoreaIT/example/JAM/exception/SQLErrorException.java
@@ -1,6 +1,7 @@
 package com.KoreaIT.example.JAM.exception;
 
 public class SQLErrorException extends RuntimeException{
+	
 	private Exception origin;
 
 	public SQLErrorException(String message, Exception origin) {
